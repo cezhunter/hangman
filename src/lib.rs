39bc1572ce52b133655b9783a5272575f7b5de6c
Cezanne Vahid <cezhunter@gmail.com>
2025-03-05 23:18:42 -0500

    moved show game into struct

--- src/lib.rs
+++ src/lib.rs
@@ -31,6 +31,7 @@ impl<'a> Game<'a> {
             status: GameStatus::Pending,
         }
     }
+
     fn register_guess(&mut self, guess: char) {
         if self.guesses.contains(&guess) || self.public_word.contains(&guess) {
             self.status = GameStatus::AlreadyGuessed(guess);
@@ -57,6 +58,26 @@ impl<'a> Game<'a> {
             }
         }
     }
+
+    fn show_game(&mut self) {
+        let message = match self.status {
+            GameStatus::AlreadyGuessed(c) => format!("already guessed {}", c),
+            GameStatus::SuccessfulGuess(n) => format!("found {} matching", n),
+            GameStatus::FailedGuess(c) => format!("Sorry, {} is not correct", c),
+            GameStatus::GameWon => String::from("Congrats! You won!"),
+            GameStatus::OutOfTurns => format!("GAME OVER. The word was {}", self.secret_word),
+            GameStatus::Pending => String::new(),
+        };
+        let guesses_string = vec_to_string(&self.guesses);
+        let word_string = vec_to_string(&self.public_word);
+        println!(
+            "Guesses: {}\n{}\n{:^19}\n{}\n",
+            guesses_string,
+            ascii::HANGMAN_ASCII[self.limbs],
+            word_string,
+            message
+        );
+    }
 }
 
 fn vec_to_string(vec: &Vec<char>) -> String {
@@ -64,26 +85,6 @@ fn vec_to_string(vec: &Vec<char>) -> String {
         .map(|c| c.to_string())
         .collect::<Vec<_>>()
         .join(" ")
-}
-
-fn show_game(game: &Game) {
-    let message = match game.status {
-        GameStatus::AlreadyGuessed(c) => format!("already guessed {}", c),
-        GameStatus::SuccessfulGuess(n) => format!("found {} matching", n),
-        GameStatus::FailedGuess(c) => format!("Sorry, {} is not correct", c),
-        GameStatus::GameWon => String::from("Congrats! You won!"),
-        GameStatus::OutOfTurns => format!("GAME OVER. The word was {}", game.secret_word),
-        GameStatus::Pending => String::new(),
-    };
-    let guesses_string = vec_to_string(&game.guesses);
-    let word_string = vec_to_string(&game.public_word);
-    println!(
-        "Guesses: {}\n{}\n{:^19}\n{}\n",
-        guesses_string,
-        ascii::HANGMAN_ASCII[game.limbs],
-        word_string,
-        message
-    );
 }
 
 fn read_words(file_path: &str) -> Vec<String> {
@@ -110,16 +111,16 @@ pub fn start_game() {
     let rand_num = rand::thread_rng().gen_range(0..words.len());
     let secret_word = &words[rand_num];
     let mut game = Game::new(secret_word);
-    println!("{}", "Welcome to Hangman! You know the rules!");
-    show_game(&game);
+    println!("Welcome to Hangman! You know the rules!");
+    game.show_game();
     loop {
-        println!("{}", "Please enter your guess.");
+        println!("Please enter your guess.");
         let guess = match get_guess() {
             Some(g) => g,
             None => continue,
         };
         game.register_guess(guess);
-        show_game(&game);
+        game.show_game();
         match &game.status {
             GameStatus::GameWon | GameStatus::OutOfTurns => break,
             _ => {}
